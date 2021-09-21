af5b75c136916b6b3248c44faae3c5d2b90619a1
Rezoanul Islam Rasel <33387505+suryabhai007@users.noreply.github.com>
2021-09-21 18:57:07 +0600

    Update settings.py
    completed

--- blogger/settings.py
+++ blogger/settings.py
@@ -1,4 +1,3 @@
-import dj_database_url
 import os
 from pathlib import Path
 
@@ -64,15 +63,16 @@ WSGI_APPLICATION = 'blogger.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+
 
 
 # Password validation
